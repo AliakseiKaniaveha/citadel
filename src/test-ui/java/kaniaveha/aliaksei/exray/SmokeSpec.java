77937ba67f18471790cc40ea53c1cdaf075778dd
Aliaksei Kaniaveha <aliaksei.kaniaveha@ukg.com>
2022-03-17 16:42:07 +0300

    Incorporate UI tests

---
+++ src/test-ui/java/kaniaveha/aliaksei/exray/SmokeSpec.java
@@ -0,0 +1,33 @@
+package kaniaveha.aliaksei.exray;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SmokeSpec {
+
+  @BeforeAll
+  static void beforeAll() {
+    Configuration.baseUrl = "http://localhost:8080";
+  }
+
+  @Test
+  void smokeTest() {
+    //when landing page loaded
+    open("/");
+
+    //then link to another page displayed
+    SelenideElement linkToAnotherPage = $(By.linkText("another page"));
+
+    //and it navigates to another page
+    linkToAnotherPage.click();
+    SelenideElement anotherPageContent = $(By.tagName("body"));
+    anotherPageContent.shouldHave(text("Hello, world!"));
+  }
+}
